42c41958b53c3f3646a26b9a3a73ca12f99e04ef
Ibrohimov <ibrohimovabdulloh573@gmail.com>
2023-12-29 22:40:00 +0500

    ok

--- src/components/Fourth/index.jsx
+++ src/components/Fourth/index.jsx
@@ -1,9 +1,33 @@
+import { useTranslation } from "react-i18next"
 import "./style.css"
 
 export function Fourth() {
+    const {t,i18n} = useTranslation()
     return(
         <div className="Fourth">
-            <h1></h1>
+            <h1>{t("Fourth.0")}</h1>
+            <div className="projects">
+                <span>
+                    <img src="https://res.cloudinary.com/ds9evspym/image/upload/v1703695179/BNBM%20trade-gypsum/2_5316851143702229533_fisaf0.png" alt="" />
+                    <h2>Glory</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias illum eaque quo delectus, obcaecati laboriosam.</p>
+                </span>
+                <span>
+                    <img src="https://res.cloudinary.com/ds9evspym/image/upload/v1703694765/BNBM%20trade-gypsum/logo_fppag4.png" alt="" />
+                    <h2>Glory</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias illum eaque quo delectus, obcaecati laboriosam.</p>
+                </span>
+                <span>
+                    <img src="https://res.cloudinary.com/ds9evspym/image/upload/v1703694765/BNBM%20trade-gypsum/logo_fppag4.png" alt="" />
+                    <h2>Glory</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias illum eaque quo delectus, obcaecati laboriosam.</p>
+                </span>
+                <span>
+                    <img src="https://res.cloudinary.com/ds9evspym/image/upload/v1703694765/BNBM%20trade-gypsum/logo_fppag4.png" alt="" />
+                    <h2>Glory</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias illum eaque quo delectus, obcaecati laboriosam.</p>
+                </span>
+            </div>
         </div>
     )
 }
