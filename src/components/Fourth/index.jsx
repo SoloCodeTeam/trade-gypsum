807c0882277ceb43efacd5bb6eb7d1992c6ab969
Ibrohimov <ibrohimovabdulloh573@gmail.com>
2023-12-31 00:58:02 +0500

    ok

--- src/components/Fourth/index.jsx
+++ src/components/Fourth/index.jsx
@@ -1,13 +1,24 @@
 import { useTranslation } from "react-i18next"
 import "./style.css"
+import { useRef } from "react"
 
 export function Fourth() {
     const {t,i18n} = useTranslation()
+    const projects = useRef()
+    // console.log(projects.current);
+    // const a = projects.current.style.left
+    // const Left = () => {
+    //     projects.current.style.left = `-30%`
+    // }
+    // const Right = () => {    
+        
+    // }
     return(
         <div className="Fourth" id="4">
             <h1>{t("Fourth.0")}</h1>
             <div className="projectsHead">
-                <div className="projects">
+                <i className="fa-solid fa-chevron-left arrows"></i>
+                <div className="projects" ref={projects} style={{left: "30%"}}>
                     <span>
                         <img src="https://www.americangypsum.com/sites/default/files/CLASSICROC-Gypsum-Wallboard-American-Gypsum.jpg" alt="" />
                         <div className="projectsTexts">
@@ -30,6 +41,7 @@ export function Fourth() {
                         </div>
                     </span>
                 </div>
+                <i className="fa-solid fa-chevron-right arrows"></i>
             </div>
         </div>
     )
