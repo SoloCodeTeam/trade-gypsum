b8b85afcb6abd6b8745f105eb78233b7c6635e38
Ibrohimov <ibrohimovabdulloh573@gmail.com>
2024-01-16 00:23:24 +0500

    ok

--- src/pages/sign/index.jsx
+++ src/pages/sign/index.jsx
@@ -1,10 +1,43 @@
-import "./style.css"
+import { useRef, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { API_URL } from '../../utils';
+import './style.css';
+import axios from "axios"
 
 export function Sign() {
-    const height = window.screen.height
-    return(
-        <div className="Sign" style={{height: `${height}px`}}>
-            
-        </div>
-    )
+  const navlink = useNavigate();
+  const namePass = useRef()
+  const namePassUp = useRef()
+  const [error, setError] = useState()
+  const HandleSubmit = async (e) => {
+    e.preventDefault()
+    const body = {
+      name: namePass.current.value,
+      password: namePassUp.current.value
+    }
+    try {
+      const res =  await axios.post(`${API_URL}/auth/login`, body)
+      setError(false) 
+      window.localStorage.setItem("AuthToken", res.data.data)
+      navlink("/admin")
+    } catch (error) {
+      setError(true)
+    }
+    }
+    const HandleNav = () => {
+        navlink('/')
+    }
+  return (
+    <div className="Sign">
+      <div className="SignPath" onClick={HandleNav}><i className='fa-solid fa-arrow-left'></i></div>
+        <form onSubmit={HandleSubmit} className="SignModal">
+            <h1>Log in</h1>
+            <p>Siz bu yerdan Admin panlega o'ta olasiz.Iltimos ism va parolni to'g'ri holatda kiriting!</p>
+            {error ? <p className='SignP'>Internet, ism yoki parolda xatolik mavjud!</p> : null}
+            <input type="text" ref={namePass} placeholder='Ismingizni kiriting' className='ins'/>
+            <input type="text" ref={namePassUp} placeholder='Parolingizni kiritng' className='ins'/>
+            <button type='submit'>Log in</button>
+        </form>
+    </div>
+  );
 }
